44dc8a161da6bbcb9108ebd29307e1e40fb7ee5e
Murilo Ribeiro <murilorsv14@gmail.com>
2021-09-07 16:35:19 -0300

    requisito 12 com feedback page

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,10 +1,12 @@
 // React
 import React, { Component } from 'react';
+import HeaderGame from '../components/HeaderGame';
 
 class Feedback extends Component {
   render() {
     return (
       <section>
+        <HeaderGame />
         <span data-testid="feedback-text">
           Texto de feedback
         </span>
