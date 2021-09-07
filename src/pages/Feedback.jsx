a0c058dff1a63b0f253c43729573a36adc63ce42
Murilo Ribeiro <murilorsv14@gmail.com>
2021-09-07 18:40:05 -0300

    requisito 14 dia7

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -17,6 +17,7 @@ class Feedback extends Component {
     super(props);
 
     this.renderMessage = this.renderMessage.bind(this);
+    this.renderFeedbackQuestion = this.renderFeedbackQuestion.bind(this);
   }
 
   renderMessage() {
@@ -28,12 +29,33 @@ class Feedback extends Component {
     return 'Mandou bem!';
   }
 
+  renderFeedbackQuestion() {
+    const { assertions } = this.props;
+    if (assertions === 0) {
+      return (
+        <span data-testid="feedback-total-question">Não acertou nenhuma pergunta</span>);
+    }
+    return (
+      <span data-testid="feedback-total-question">
+        Acertou
+        {assertions}
+        perguntas
+      </span>);
+  }
+
   render() {
+    const { score } = this.props;
     return (
       <section>
         <HeaderGame />
         <span data-testid="feedback-text">
           { this.renderMessage() }
+        </span>
+        <span data-testid="feedback-total-score">
+          { score }
+        </span>
+        <span data-testid="feedback-total-question-span">
+          { this.renderFeedbackQuestion() }
         </span>
         <PlayAgain />
         <ViewRanking />
@@ -44,10 +66,12 @@ class Feedback extends Component {
 
 Feedback.propTypes = {
   assertions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
 };
 
 const mapStateToProps = (store) => ({
   assertions: store.game.assertions, // Número de acertos
+  score: store.header.score,
 });
 
 export default connect(mapStateToProps, null)(Feedback);
