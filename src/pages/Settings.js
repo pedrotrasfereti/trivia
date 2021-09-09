00cd2239d5222c33830a4f4cdf01cf85ee1163f2
deyvid-dev <deyvidscm@hotmail.com>
2021-09-09 19:30:39 -0300

    req bonus

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,10 +1,44 @@
 import React from 'react';
 
 class Settings extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   render() {
     return (
       <div>
         <h1 data-testid="settings-title">Configurações</h1>
+        <label htmlFor="value">
+          Quantidade:
+          <select id="value" name="quatidade" onChange={ this.handleChange }>
+            <option>5</option>
+            <option>10</option>
+            <option>15</option>
+            <option>20</option>
+          </select>
+        </label>
+        <label htmlFor="value">
+          Categoria:
+          <select id="value" name="categoria" onChange={ this.handleChange }>
+            <option>categorias</option>
+          </select>
+        </label>
+        <label htmlFor="value">
+          Dificuldade:
+          <select id="value" name="dificuldade" onChange={ this.handleChange }>
+            <option>Qualquer</option>
+            <option>Fácil</option>
+            <option>Normal</option>
+            <option>Dificil</option>
+          </select>
+        </label>
       </div>
     );
   }
